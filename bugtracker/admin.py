91b6e98a0e6064811f3bc55a8eb3ea917efb450f
Stanislav Fateev <fateevstas@yandex.ru>
2014-09-03 09:49:23 +0400

    Added ListView and templates for it

--- bugtracker/admin.py
+++ bugtracker/admin.py
@@ -2,4 +2,9 @@
 from django.contrib import admin
 from .models import Ticket
 
-admin.site.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('closed', 'title', 'text', 'created', 'user')
+    list_filter = ['created', 'closed']
+    search_fields = ['title', 'text']
+
+admin.site.register(Ticket, TicketAdmin)
