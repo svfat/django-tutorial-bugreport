3efde230ceb06e1fef0fb5843d28d91a60fb8b40
Stanislav Fateev <fateevstas@yandex.ru>
2014-09-03 09:55:18 +0400

    Added DetailView and template for it

--- bugtracker/urls.py
+++ bugtracker/urls.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 from django.conf.urls import patterns, include, url
-from .views import BugListView
+from .views import BugListView, BugDetailView
 
 urlpatterns = patterns('',
     url(r'^$', BugListView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', BugDetailView.as_view(), name='detail'),
 )
