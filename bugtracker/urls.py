f2058d2cb3dc9a4443ebe0c12474f6974c5a289d
Stanislav Fateev <fateevstas@yandex.ru>
2014-09-03 09:49:23 +0400

    Added ListView and templates for it

--- bugtracker/urls.py
+++ bugtracker/urls.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 from django.conf.urls import patterns, include, url
+from .views import BugListView
 
 urlpatterns = patterns('',
+    url(r'^$', BugListView.as_view(), name='index'),
 )
--- bugtracker/views.py
+++ bugtracker/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Ticket
 
-# Create your views here.
+class BugListView(ListView):
+    model = Ticket
+    template_name = 'list.html'
