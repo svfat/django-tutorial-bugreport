3dce830d449c618861be0cbfd6080cbadf4b851f
Stanislav Fateev <fateevstas@yandex.ru>
2014-09-03 09:55:18 +0400

    Added DetailView and template for it

--- bugtracker/views.py
+++ bugtracker/views.py
@@ -1,6 +1,12 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Ticket
 
 class BugListView(ListView):
     model = Ticket
     template_name = 'list.html'
+
+
+
+class BugDetailView(DetailView):
+    model = Ticket
+    template_name = 'detail.html'
